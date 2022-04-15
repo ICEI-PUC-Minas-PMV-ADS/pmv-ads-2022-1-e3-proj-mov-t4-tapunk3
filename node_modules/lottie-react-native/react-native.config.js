ca51849a1324150380666128e8cb547bf974647d
Rodrigo Charles <rcharlescolors@gmail.com>
2022-04-15 09:45:22 -0300

    splahpage

---
+++ node_modules/lottie-react-native/react-native.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        sourceDir: './src/android',
+      },
+    },
+  },
+  project: {
+    android: {
+      sourceDir: './example/android',
+    },
+  },
+};
