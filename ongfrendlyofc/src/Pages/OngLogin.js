6d1c32e39e23026e2bf2713901732c495a03b31f
Rodrigo Charles <rcharlescolors@gmail.com>
2022-06-07 17:28:05 -0300

    ajuste no metodo de login

--- ongfrendlyofc/src/Pages/OngLogin.js
+++ ongfrendlyofc/src/Pages/OngLogin.js
@@ -1,69 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Image, AsyncStorage, Alert } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 
-import  AsyncStorage  from '@react-native-async-storage/async-storage';
 import {useNavigation} from '@react-navigation/native';
+import { NavigationActions, StackActions } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
+
 import {useUser} from '../contexts/UserContext';
 import { login } from '../services/authService';
-import { getUser, insertUser } from './localLoginService';
-
-import { useIsFocused } from '@react-navigation/native'; 
 
 const LoginOng = () => {
 
   const navigation = useNavigation();
-  const isFocused = useIsFocused();
-  //const {setSigned, setName} = useUser(); // este cara está quebrando a aplicaçao verificar com o monitor.
+  //const {setSigned, setName} = useUser();
+
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // usado no sqllite
-  //const [user, setUser] = useState([]);
 
-/*   useEffect(() => {
-    getUser().then(dados => {
-      console.log(dados);
-      setUser(dados);
-      });
-  }, [isFocused]);
- */
-/*   const handleLogin = ( ) => {
-    insertUser({
-      email: email,
-      password: password, 
-    }).then( res => {
-      console.log(res);
-
-      if(res && res.user){
-        setSigned(true);
-        setName(res.user.name);
-        AsyncStorage.setItem('@TOKEN_KEY', res. accessToken).then();
-        navigation.navigate('Ong Atividade');
-      }else{
-        Alert.alert('Atenção', 'Usuário ou senha inválidos!');
-      }
-    });
-  } */
-
-  // metodo usando o jsonserver
-
-/*   const handleLogin = ( ) => {
+  const handleLogin = ( ) => {
     login({
       email: email,
       password: password
     }).then( res => {
-      console.log(res);
+      console.log(res)
+      navigation.navigate('HomeOng');
 
-      if(res && res.user){
-        setSigned(true);
-        setName(res.user.name);
-        AsyncStorage.setItem('@TOKEN_KEY', res. accessToken).then();
+      /* if(res && res.user){
+      setSigned(true);
+      setName(res.user.name);
+       AsyncStorage.setItem('@TOKEN_KEY', res.accessToken).then();
       }else{
         Alert.alert('Atenção', 'Usuário ou senha inválidos!');
-      }
+      } */
     });
-  } */
+  }
 
   return (
     <>
@@ -76,21 +47,19 @@ const LoginOng = () => {
           style={styles.input}
           label="E-mail"
           value={email}
-          //value={user.email}
           onChangeText={(text) => setEmail(text)}
         />
         <TextInput
           style={styles.input}
           label="Senha"
           value={password}
-          //value={user.password}
           secureTextEntry
           onChangeText={(text) => setPassword(text)}
         />
         <Button
           style={styles.button}
           mode="contained"
-          onPress={() =>navigation.navigate('HomeOng')}>
+          onPress={handleLogin}> 
           Entrar
         </Button>
         <Button
